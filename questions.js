a79f7f4383e3598d38aee1aafd46c28b1de13d77
Benji <snowrepor@gmail.com>
2021-11-20 14:22:58 -0700

    Added questions

---
+++ questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["booleans", "numbers and strings", "other arrays", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["parenthesis", "quotes", "curly brackets", "commas"],
+        answer: "quotes"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____..",
+        choices: ["square brackets", "parentheses", "curly brackets", "quotes"],
+        answer: "parentheses"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["terminal / bash", "Javascript", "console log", "for loops"],
+        answer: "console log"
+    },
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+
+];
+
+var score = 0;
+var questionIndex = 0;
